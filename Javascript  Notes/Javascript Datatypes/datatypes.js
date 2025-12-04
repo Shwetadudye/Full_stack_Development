11000cc121ee4d8b59a5119791891fc180937136
Shweta Dudye ✅ <dudyeshweta@gmail.com>
2025-12-04 15:20:40 +0530

    update

---
+++ Javascript  Notes/Javascript Datatypes/datatypes.js
@@ -0,0 +1,14 @@
+/*
+format of data in js
+*/
+//"name"  string 
+//00  number 
+//true/false  boolean
+// [cs,pubg,coc]  array /collection
+// {name, age, address,contact }  object/document
+// undefined  undefined
+// null  null
+//big-IN     9.98982598634899
+
+
+//[{},{},{}]  collection of documents mangodb 
