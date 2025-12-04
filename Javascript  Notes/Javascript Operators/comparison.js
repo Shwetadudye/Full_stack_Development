c3c4423a70811e15f8247bdde7ac65b20bc1a8f4
Shweta Dudye ✅ <dudyeshweta@gmail.com>
2025-12-04 21:01:32 +0530

    Create comparison.js

---
+++ Javascript  Notes/Javascript Operators/comparison.js
@@ -0,0 +1,24 @@
+// Comparison Operators 
+// Equal to ==
+// Not Equal to !=
+// Equal to & type ===
+// Not Equal to and type !==
+
+//>
+//>=
+// <
+//<=
+
+//let  a = 8;
+//let b = 4;
+//console.log("a==b", a==b);
+//console.log ("a!=b", a!=b);
+
+let a = " 23";
+let b = 23 ;
+//console.log("a ===b ", a===b);
+//console.log("a !==b", a!==b):
+//console.log("a>b",a>b);
+//console.log("a<b", a<b);
+//console.log("a>=b",a>=b);
+//console.log ("a<=b",a<=b);
