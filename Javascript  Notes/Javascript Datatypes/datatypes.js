8c37011f7b6ee2d37021db58a72f1f8259fa6f05
Shweta Dudye ✅ <dudyeshweta@gmail.com>
2025-12-04 15:43:28 +0530

    Update datatypes.js

--- Javascript  Notes/Javascript Datatypes/datatypes.js
+++ Javascript  Notes/Javascript Datatypes/datatypes.js
@@ -1,14 +1,19 @@
-/*
+/* primitive data types 
+primitive data types are a set of basic data types in js
+object is a non primitive data types
+
 format of data in js
 */
+//this are the primitibe data types 
 //"name"  string 
 //00  number 
 //true/false  boolean
 // [cs,pubg,coc]  array /collection
-// {name, age, address,contact }  object/document
 // undefined  undefined
 // null  null
 //big-IN     9.98982598634899
 
+
+// {name, age, address,contact }  object/document
 
 //[{},{},{}]  collection of documents mangodb 
