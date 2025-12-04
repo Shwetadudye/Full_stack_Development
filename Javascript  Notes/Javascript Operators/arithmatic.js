fb194a83f8b97c1c2a984674b548e532629e4370
Shweta Dudye ✅ <dudyeshweta@gmail.com>
2025-12-04 16:00:40 +0530

    Create arithmatic.js

---
+++ Javascript  Notes/Javascript Operators/arithmatic.js
@@ -0,0 +1,15 @@
+/* Arithemtic operators */ // done
+
+let n1 = 3;
+let n2 = 4;
+
+let mul = n1 * n2;
+let dev = n1 / n2;
+let sum1 = n1 + n2;
+let sub1 = n1 - n2;
+let expo = n1 ** n2;
+console.log('expo:', expo);
+// console.log(' sum1:', sum1);
+// console.log(' sub1:', sub1);
+// console.log(' dev:', dev);
+// console.log(' mul:', mul);
