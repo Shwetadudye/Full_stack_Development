95616d71ed2b177b254f62607aabe8004d76d89b
Shweta Dudye ✅ <dudyeshweta@gmail.com>
2025-12-04 21:15:05 +0530

    Create logical.js

---
+++ Javascript  Notes/Javascript Operators/logical.js
@@ -0,0 +1,17 @@
+// Logical operators 
+// AND &&
+// OR ||
+// NOT !
+
+
+ let num1 = 3;
+ let num2 = 3;
+
+  let num4 = 3;
+  let num5 = 3;
+
+// console.log(num1 + 1 == num2 && num4 == num5);
+// console.log(true && true);
+
+// console.log(num1 == num2 || num4 != num5);
+// console.log(true || false);
