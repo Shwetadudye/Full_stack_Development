2359425bae6e1a4be5d96b503c924aa2be52c721
Shweta Dudye ✅ <dudyeshweta@gmail.com>
2025-12-04 20:34:56 +0530

    update

---
+++ Javascript  Notes/Javascript Operators/assignment.js
@@ -0,0 +1,13 @@
+//assignments operators 
+//assign values
+
+let a =5 ;
+let b =8 ;
+a += 4; // a = a+ 4
+
+//a -= 4;  a = a-4 
+//a*=4;  a = a*4
+// a %= a ; a = a/4
+// a **= a ; a = a**4
+
+console.log( " a = ", a);
