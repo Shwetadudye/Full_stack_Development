b9bccc3a39094d0f4d6bf3faa2f12bac08b9f27d
Shweta Dudye ✅ <dudyeshweta@gmail.com>
2025-12-05 20:53:51 +0530

    Create strings.js

---
+++ Javascript  Notes/Javascript Operators/strings.js
@@ -0,0 +1,29 @@
+/* String operators */
+
+/*  
+
++ => concatination 
++= => Append string
+
+*/
+
+let name8 = 'Shweta';
+
+let sur_name = 'Dudya';
+
+let full_name = name8 + ' ' + sur_name;
+
+let maried = false;
+
+let answer = `${name8} ${sur_name} is not maried ${maried}`;
+
+// console.log(' answer:', answer);
+
+let name1 = 'Shweta';
+let name2 = 'Dudye';
+
+// let full_name = name1 + " " + name2;
+// console.log(" full_name:", full_name);
+
+name1 += ' ' + name2;
+// console.log(' name1:', name1);
