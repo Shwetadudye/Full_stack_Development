c10e598f1e0b895437d8e9afa0af8842ba2b3f15
Shweta Dudye ✅ <dudyeshweta@gmail.com>
2025-12-05 20:43:30 +0530

    Create bitwise.js

---
+++ Javascript  Notes/Javascript Operators/bitwise.js
@@ -0,0 +1,30 @@
+
+/* 
+-> binary-system/number-system( check bitwise image)
+
+& -> AND
+| -> OR
+~ -> NOT
+
+*/
+
+let num66 = 1;
+
+let num77 = 14;
+
+let binaryConv1 = num66.toString(2);
+
+// console.log(' binaryConv:', binaryConv1);
+
+let binaryConv2 = num77.toString(2);
+
+// console.log(' binaryConv:', binaryConv2);
+
+ let and = num66 & num77;
+console.log(" and:", and);
+
+
+//let OR = num66 || num77;
+
+//let NOT= ~num77;
+//console.log('NOT',NOT);
