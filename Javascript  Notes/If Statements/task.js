4b800d5b44ce3697d9c086000adb263b8c44b6ac
Shweta Dudye ✅ <dudyeshweta@gmail.com>
2025-12-06 16:46:06 +0530

    Create task.js

---
+++ Javascript  Notes/If Statements/task.js
@@ -0,0 +1,58 @@
+//Check if a number is **even or odd**.
+
+/*let num = 12 ;
+
+if (num % 2 ==0){
+    console.log("Number is even");
+}
+else {
+    console.log("Number is odd");
+}
+    */
+
+// Check if a number is **positive, negative, or zero**.
+
+ /*let num = 1 ;
+
+ if (num <0 ){
+    console.log (num +"is negative");
+ }
+ else if(num >0) {
+    console.log (num +" is positive");
+ }
+ else{
+    console.log ("zero");
+ }*/
+
+   /* // Find the **greater** between two numbers.
+
+    let a = 45;
+    let b = 15;
+
+    if(a>b){
+        console.log("a is greater then b");
+    }
+    else {
+        console.log("b is greater than a")
+    }
+  */
+
+
+    //Find the **largest among three numbers**.
+  /*  let num1=105;
+    let num2 = 54;
+    let num3 =98 ;
+
+    if(num1>num2 &&num1>num2){
+        console.log(num1 + "is largest");
+    }
+    else if (num2> num1 && num2> num3){
+        console.log(num2 +"is largest");
+    }
+    else {
+        console.log(num3 +"is largest")
+    }*/
+
+        // Check if a given **year is a leap year** or not.
+        
+
