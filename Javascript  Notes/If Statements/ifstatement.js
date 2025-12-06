f6b73a2e5991aa8d76b6cda957ce544223bbea55
Shweta Dudye ✅ <dudyeshweta@gmail.com>
2025-12-06 15:37:17 +0530

    Create ifstatement.js

---
+++ Javascript  Notes/If Statements/ifstatement.js
@@ -0,0 +1,26 @@
+
+// if statements
+
+// if - else statements
+
+/*let age = 17;
+
+if(age>18) {
+  console.log ("You can drive");
+}
+else{
+    console.log("You cannot drive");
+}*/
+
+// if else if statements
+let AGE = 85;
+
+if(AGE >=18 && AGE<56){
+    console.log("Eligible for vote");  
+}
+else if(AGE>=56 && AGE<75){
+    console.log("Null for vote");
+}
+else{
+  console.log("Not eligible fot vote");
+}
