d86e35f7f4987912c471709a18803498979d4a86
Shweta Dudye ✅ <dudyeshweta@gmail.com>
2025-12-06 21:31:26 +0530

    Switch case

---
+++ Javascript  Notes/Switch Case/switch.js
@@ -0,0 +1,16 @@
+let AGE = 100;
+
+switch (true) {
+    case AGE >= 18 && AGE < 56:
+        console.log('Eligible for vote');
+        break;
+         case AGE >= 56 && AGE < 75:
+        console.log('Null for vote');
+        break;
+         case AGE >= 18:
+        console.log(' Not Eligible for vote');
+        break;
+    default:
+        console.log('key was not matched');
+        break;
+}
