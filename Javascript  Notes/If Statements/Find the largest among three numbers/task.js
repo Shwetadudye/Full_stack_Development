c5b3cc33d96c5be2505d4d0cdb70f8f59397e5ee
Shweta Dudye ✅ <dudyeshweta@gmail.com>
2025-12-07 15:58:01 +0530

    Create task.js

---
+++ Javascript  Notes/If Statements/Find the largest among three numbers/task.js
@@ -0,0 +1,13 @@
+let num1=105;
+    let num2 = 54;
+    let num3 =98 ;
+
+    if(num1>num2 &&num1>num2){
+        console.log(num1 + "is largest");
+    }
+    else if (num2> num1 && num2> num3){
+        console.log(num2 +"is largest");
+    }
+    else {
+        console.log(num3 +"is largest")
+    }
