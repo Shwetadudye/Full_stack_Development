c3447cd8cea0c8ee7ffac6c2df5ad5657b7787c5
Shweta Dudye ✅ <dudyeshweta@gmail.com>
2025-12-07 15:57:48 +0530

    Create loops.js

---
+++ Javascript  Notes/Loops in js/loops.js
@@ -0,0 +1,15 @@
+// for loop 
+
+/*For loop ==> It is a type of loop. It is used when we know how many times code will be run. In this loop we have three parts. First one is initialization, in this part we initialized the itrator. Second part is condition in which we write a condition according to the questions. And last part is updation in which we write either increament or decreament according to the given questions.
+
+syntax
+
+for(initialization; condition; updation){
+    code
+}*/
+
+
+let a = 1;
+for (let i = 0 ; i<100 ; i++){
+    console.log(a+i);
+}
