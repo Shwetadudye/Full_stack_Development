ac8ca3315eff57bab99e5f52ef476d46a7d8afb4
Shweta Dudye ✅ <dudyeshweta@gmail.com>
2025-12-07 16:27:11 +0530

    Create task.js

---
+++ Javascript  Notes/Switch Case/Input a number (1–4). Print/task.js
@@ -0,0 +1,25 @@
+                                // Input a number (1–4). Print:
+   /*- 1 → Addition
+   - 2 → Subtraction
+   - 3 → Multiplication
+   - 4 → Division */
+
+    let num = 9 ;
+
+   switch(num){
+    case 1:
+        console.log("Addition");
+        break;
+        case 2:
+            console.log("Substraction");
+            break;
+            case 3:
+                console.log("Multiplication");
+                break;
+                case 4 :
+                    console.log("Division");
+                    break;
+                    default:
+                        console.log("Invalid input .... Please enter num between 1 to 4");
+                        break;
+   } 
