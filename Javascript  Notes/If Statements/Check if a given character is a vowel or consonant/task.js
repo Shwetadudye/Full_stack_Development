b6aedbfb419f178cf45c3199c56977bc10aee2b4
Shweta Dudye ✅ <dudyeshweta@gmail.com>
2025-12-07 16:15:48 +0530

    Create task.js

---
+++ Javascript  Notes/If Statements/Check if a given character is a vowel or consonant/task.js
@@ -0,0 +1,10 @@
+let char = 'A' ;
+
+   char = char.toLowerCase(); 
+
+   if (char=== 'a' || char ==='e' || char ==='i'|| char ==='o'||char ==='u'){
+    console.log (char+" is vowel");
+   }
+   else {
+    console.log (char + "is consonant");
+   }
