a818cca08964179d3e4bd70e0c835dc7dbd99e17
Shweta Dudye ✅ <dudyeshweta@gmail.com>
2025-12-08 21:08:02 +0530

    Update loops.js

--- Javascript  Notes/Loops in js/loops.js
+++ Javascript  Notes/Loops in js/loops.js
@@ -9,7 +9,51 @@ for(initialization; condition; updation){
 }*/
 
 
-let a = 1;
+/*let a = 1;
 for (let i = 0 ; i<100 ; i++){
     console.log(a+i);
+}*/
+
+// for in loop 
+/*let obj ={
+    name : "shweta",
+    age : "23",
+    pass: "yes",
 }
+
+for (const key in obj) {
+ 
+    const element = obj[key];
+    console.log(element);
+    
+    
+}*/
+
+
+// for of loop 
+
+/*let obj={
+    name : "shweta",
+}
+
+for (const c of "shweta") {
+    console.log(c);
+}
+    */
+
+
+// While loop imp 
+
+/*let i =0;
+
+while(i<6){
+    console.log(i)
+    i++;
+}*/
+
+// do while loop
+let i =10;
+do {
+    console.log(i)
+    i++;
+} while (i<6);
