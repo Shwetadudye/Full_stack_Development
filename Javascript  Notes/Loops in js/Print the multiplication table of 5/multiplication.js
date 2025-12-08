3615aef3b3530c007eb21b2691249c024f691fc2
Shweta Dudye ✅ <dudyeshweta@gmail.com>
2025-12-08 21:30:03 +0530

    Create multiplication.js

---
+++ Javascript  Notes/Loops in js/Print the multiplication table of 5/multiplication.js
@@ -0,0 +1,10 @@
+//Print the multiplication table of 5.
+
+ /* for (let i =1; i<=10;i++){
+    console.log(i*5);
+  }*/
+
+    for (let i = 1; i <= 10; i++) {
+   console.log("5 x " + i + " = " + (5*i));
+   }
+  
