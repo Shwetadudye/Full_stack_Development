de4eff8b248c25482c1b953928a3306c16816ec7
Shweta Dudye ✅ <dudyeshweta@gmail.com>
2026-01-02 10:25:41 +0530

    update

--- Javascript  Notes/Loops in js/While loop/Check if a given number is prime using loops/task.js
+++ Javascript  Notes/Loops in js/While loop/Check if a given number is prime using loops/task.js
@@ -1,3 +1,18 @@
 //Check if a given number is prime using loops
 
-
+let num = 3;
+let i =2;
+let isprime = true;
+if(num <=1){
+    isprime = false;
+}else{
+    while(i<num){
+        if(num % i===0){
+        isprime= false;
+        break;
+        }
+        i++;
+    }
+   
+}
+console.log(isprime ? "Prime number" : "Not Prime number");
