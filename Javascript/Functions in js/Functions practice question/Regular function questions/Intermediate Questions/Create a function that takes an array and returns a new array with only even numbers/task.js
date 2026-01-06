b201dceaac5b13432bb7b83206f8915b57c9a050
Shweta Dudye <dudyeshweta@gmail.com>
2026-01-06 16:18:08 +0530

    Intermediate question function

---
+++ Javascript/Functions in js/Functions practice question/Regular function questions/Intermediate Questions/Create a function that takes an array and returns a new array with only even numbers/task.js
@@ -0,0 +1,18 @@
+//Create a function that takes an array and returns a new array with only even numbers
+
+function getEvenNumbers(arr) {
+  let result = [];
+  let j = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      result[j] = arr[i];
+      j++;
+    }
+  }
+
+  return result;
+}
+console.log(getEvenNumbers([1, 2, 3, 4, 5, 6]));
+
+// [2, 4, 6]
