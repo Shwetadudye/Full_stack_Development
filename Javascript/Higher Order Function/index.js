daa56e64b48d7afd77a9026e5cdf5957f7a2f564
Shweta Dudye <dudyeshweta@gmail.com>
2026-01-08 17:39:28 +0530

    Code of the day

---
+++ Javascript/Higher Order Function/index.js
@@ -0,0 +1,15 @@
+let number="hellooo"
+let newArr=Array.from(number,(_,i)=>i+1)
+console.log(newArr);
+
+let data = newArr.map((element,index,array)=>element<5?array[index]:null)
+console.log(data)
+
+let str=["Mango","apple","Kiwi","Watermelon","mango","apple","kiwi","banana","lichi","muskmelon"]
+
+let data1 = str.filter((element,index,array)=>index!=0)
+console.log(data1)
+
+let data3=str.forEach((el,i,arr) => {
+    el==="apple"?console.log(el,i):null
+});
