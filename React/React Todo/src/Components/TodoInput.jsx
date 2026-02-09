5201eedc25082dc79285478ac7a6b7d9681b093c
Shweta Dudye <dudyeshweta@gmail.com>
2026-02-09 10:55:31 +0530

    React Todo

--- React/React Todo/src/Components/TodoInput.jsx
+++ React/React Todo/src/Components/TodoInput.jsx
@@ -18,15 +18,16 @@ export const TodoInput = () => {
 
         setTodo((prev) =>[...prev, data]);
         console.log(todo); // interVIEW QUESTION IDHAR TODO [] EMPTY AA RAHA HAI KYUKI ASYNC FUNCTION HAI 
+        setText('');
     };
      console.log(todo);
 
      return(
         <>
-        <input type="text" onChange={(e)=>setText(e.target.value)}/>
+        <input value={text} name="todo_input"type="text" onChange={(e)=>setText(e.target.value)}/>
         <button onClick={handleAdd}>Add</button>
-        <Todos props={{todo}} />
-        
+        <Todos props={{todo, setTodo}} />
+      
         </>
      );
  };
--- React/React Todo/src/Components/Todos.jsx
+++ React/React Todo/src/Components/Todos.jsx
@@ -1,9 +1,103 @@
-export const Todos = ({props}) => {
-    const {todo} = props;
-    console.log('Todo List comp' , todo);
-    return(
-        <>
-          <h1>list of todos</h1>
-        </>
+import { useState } from 'react';
+
+export const Todos = ({ props }) => {
+  const [editText, setEditText] = useState('');
+
+  const { todo, setTodo } = props;
+
+  const handleDelete = (id) => {
+    const deleteItems = todo.filter((el) => el.id !== id);
+    setTodo(deleteItems);
+  };
+
+  const handleEdits = (id) => {
+    const editItems = todo.map((el) =>
+      el.id === id ? { ...el, isEdit:!el.isEdit } : el,
     );
+    setTodo(editItems);
+  };
+
+  const handleCancel = (id) => {
+    const editItems = todo.map((el) =>
+      el.id === id ? { ...el, isEdit: false } : el,
+    );
+    setTodo(editItems);
+  };
+
+  const handleConfirm = (id) => {
+    if (editText.trim() === '') return;
+    const editItems = todo.map((el) =>
+      el.id === id ? { ...el, isEdit: false, text: editText } : el,
+    );
+    setTodo(editItems);
+  };
+  return (
+    <>
+      <h1>list of todos</h1>
+
+      {/* {
+        todo && 
+      } */}
+
+      {todo.map((el) => {
+        return (
+          <div
+            key={el.id}
+            style={{
+              width: '80%',
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              margin: 'auto',
+            }}
+          >
+            <input type="checkbox" />
+
+            {el.isEdit ? (
+              <input
+                name="edit_items"
+                type="text"
+                defaultValue={el.text}
+                onChange={(e) => setEditText(e.target.value)}
+              />
+            ) : (
+              <h1>{el.text}</h1>
+            )}
+
+            {el.isEdit ? (
+              <>
+                <button
+                  onClick={() => handleCancel(el.id)}
+                  style={{ height: 'fit-content' }}
+                >
+                  cancel
+                </button>
+                <button
+                  onClick={() => handleConfirm(el.id)}
+                  style={{ height: 'fit-content' }}
+                >
+                  confirm
+                </button>
+              </>
+            ) : (
+              <>
+                <button
+                  onClick={() => handleEdits(el.id)}
+                  style={{ height: 'fit-content' }}
+                >
+                  edit
+                </button>
+                <button
+                  onClick={() => handleDelete(el.id)}
+                  style={{ height: 'fit-content' }}
+                >
+                  delete
+                </button>
+              </>
+            )}
+          </div>
+        );
+      })}
+    </>
+  );
 };
