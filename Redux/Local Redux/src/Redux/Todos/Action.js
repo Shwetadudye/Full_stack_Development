f3e1599f2a2f2cb9e380234ae6ca04620550eeb9
Shweta Dudye <dudyeshweta@gmail.com>
2026-03-20 10:34:30 +0530

    Code of the day

--- Redux/Local Redux/src/Redux/Todos/Action.js
+++
@@ -1,4 +0,0 @@
-export const ADD_TODOS = 'ADD_TODOS';
-export const EDIT_TODOS = 'EDIT_TODOS';
-export const DELETE_TODOS ='DELETE_TODOS';
-export const COMPLETE_TODOS = 'COMPLETE_TODOS'
---
+++ Redux/local_redux/src/Pages/PageNotFound404.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const PageNotFound404 = () => {
+  return <div>PageNotFound404</div>;
+};
